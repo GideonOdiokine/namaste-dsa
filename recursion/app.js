3a9af1bfa2998834de6b27c499529d0f7db3e7e0
gideonodiokine <gidysmartodiokineh@gmail.com>
2025-07-12 19:38:28 +0100

    feat: sumOddNumber

--- recursion/app.js
+++ recursion/app.js
@@ -16,14 +16,30 @@ function sumArry(n) {
   return arr[n] + sumArry(n - 1);
 }
 
+
+// Exercise
+function sumOddNumber(n) {
+  let isOdd = arr[n] % 2 != 0;
+  if (n == 0) {
+    if (isOdd) {
+      return arr[n];
+    } else return 0;
+  }
+
+  if (isOdd) {
+    return arr[n] + sumOddNumber(n - 1);
+  } else {
+    return 0 + sumOddNumber(n - 1);
+  }
+}
+
 // here is the tracing base on my understanding;
 // sumArry(4)
-    //  arr(4) + sumArry(4 - 1) => 1 + sumArry(3);
-                //  sumArry(3) => 0 + sumArry(2)
-                                    // sumArry(2)=> 2 + sumArry(1)
-                                                        // sumArry(1) => 3 + sumArry(0)
-                                                                            // sumArry(0)=> 5
+//  arr(4) + sumArry(4 - 1) => 1 + sumArry(3);
+//  sumArry(3) => 0 + sumArry(2)
+// sumArry(2)=> 2 + sumArry(1)
+// sumArry(1) => 3 + sumArry(0)
+// sumArry(0)=> 5
 // result => 1 + 0 + 2+ 3+ 5
 
-
-console.log(sumArry(arr.length - 1));
+console.log(sumOddNumber(arr.length - 1));
