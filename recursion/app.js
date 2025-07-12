95ac1d2d2d2f161a9517fe22f276b73cff4aa59a
gideonodiokine <gidysmartodiokineh@gmail.com>
2025-07-12 16:46:21 +0100

    perf: tracing the funct

--- recursion/app.js
+++ recursion/app.js
@@ -1,20 +1,29 @@
 // Sum of first n numbers
 
-function sum(n) {
-  if (n == 0) return 0;
-  return n + sum(n - 1);
-}
+// function sum(n) {
+//   if (n == 0) return 0;
+//   return n + sum(n - 1);
+// }
 
-console.log(sum(5));
+// console.log(sum(5));
 
 // sum of all elements in an array
-let arr = [5, 3, 1, 2, 4];
+let arr = [5, 3, 2, 0, 1];
 function sumArry(n) {
   if (n == 0) {
     return arr[n];
   }
-  return arr[n] + sumArry(arr[n] - 1);
-
+  return arr[n] + sumArry(n - 1);
 }
+
+// here is the tracing base on my understanding;
+// sumArry(4)
+    //  arr(4) + sumArry(4 - 1) => 1 + sumArry(3);
+                //  sumArry(3) => 0 + sumArry(2)
+                                    // sumArry(2)=> 2 + sumArry(1)
+                                                        // sumArry(1) => 3 + sumArry(0)
+                                                                            // sumArry(0)=> 5
+// result => 1 + 0 + 2+ 3+ 5
+
 
 console.log(sumArry(arr.length - 1));
